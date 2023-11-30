b1f572cbe82f16b07c35c2a646140928734d6f90
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-30 18:58:58 -0500

    documenting

--- src/database/database.py
+++ src/database/database.py
@@ -1,3 +1,8 @@
+# This line imports the SQLAlchemy class from the flask_sqlalchemy package.
+#flask_sqlalchemy is a Flask extension that simplifies database integration with Flask applications.
 from flask_sqlalchemy import SQLAlchemy
 
+
+#this creates an instance of the SQLAlchemy class and assigns it to the variable db
+#This  instance represents the database connection and provides methods and attrobutes to interact with the dB
 db = SQLAlchemy()
