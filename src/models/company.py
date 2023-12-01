d61ff40e7d7e5e2d78f6836a79c94cc0d95095bc
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-01 17:23:56 -0500

    Documenting

--- src/models/company.py
+++ src/models/company.py
@@ -12,6 +12,6 @@ class Company(db.Model):
 
     __tablename__ = "company"
 
-    company_id = db.Column(db.Integer, primary_key=True)
+    company_id = db.Column(db.Integer, primary_key=True) 
     name = db.Column(db.String(255))
-    #user_company_mapping = db.relationship('UserCompanyMapping', backref='company', lazy=True)
+    #user_company_mapping = db.relationship('UserCompanyMapping', backref='company', lazy=True) 
