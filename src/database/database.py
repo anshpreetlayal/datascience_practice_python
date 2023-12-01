18e258d0cf07847a1c3e16703879226b48e69b5f
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-30 19:24:15 -0500

    documenting

--- src/database/database.py
+++ src/database/database.py
@@ -9,17 +9,6 @@ a Flask extension simplifying database interactions.
 
 SQLAlchemy provides methods and attributes to interact with the database, including defining models,
 performing CRUD operations, and managing database transactions in a Flask application.
-
-Usage Example:
-    # Define a database model using 'db' object
-    class User(db.Model):
-        # Define User model fields and relationships here
-        ...
-
-    # Create and manipulate database objects using 'db' methods
-    user = User(username='example', email='example@example.com')
-    db.session.add(user)
-    db.session.commit()
 """
 from flask_sqlalchemy import SQLAlchemy
 
